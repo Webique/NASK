8d5a14ee752c95a5006f8426b6643e7f1d1177c5
vedaluka <vedaluka@users.noreply.github.com>
2025-12-18 07:53:34 +0000

    Update global contact details

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,14 +3,14 @@ export const siteConfig = {
     instagram: "https://www.instagram.com/nask_nanofiber",
     linkedin: "https://www.linkedin.com/company/nask-nanofiber",
     x: "https://x.com/nask_nanofiber",
-    whatsapp: "https://wa.me/966568081744"
+    whatsapp: "https://wa.me/966504563212"
   },
 
   support: {
-    phone: "+966 56 808 1744",
-    email: "monzer@sarabeelmedical.com",
-    email2: "raghad@sarabeelmedical.com",
-    whatsapp: "+966568081744"
+    phone: "+966 504563212",
+    email: "info@nask-mena.com",
+    email2: "sales@nask-mena.com",
+    whatsapp: "+966504563212"
   },
 
   url: process.env.NEXT_PUBLIC_SITE_URL!,
