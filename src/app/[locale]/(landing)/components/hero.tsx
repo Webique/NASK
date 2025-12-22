2baae9d87fc3a6042b7f3af4a26d6e13bd486b59
vedaluka <vedaluka@users.noreply.github.com>
2025-12-22 06:40:31 +0000

    changess

--- src/app/[locale]/(landing)/components/hero.tsx
+++ src/app/[locale]/(landing)/components/hero.tsx
@@ -52,7 +52,7 @@ export default function Hero() {
           alt="NASK Logo"
           width={800}
           height={800}
-          className="h-96 w-auto lg:h-[32rem] xl:h-[40rem] 2xl:h-[48rem] pointer-events-none"
+          className="h-80 w-auto lg:h-[28rem] xl:h-[36rem] 2xl:h-[42rem] pointer-events-none"
           priority
         />
       </m.div>
